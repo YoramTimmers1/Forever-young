13b0e14a58834faf4f335c1481a733de8f48c797
YoramTimmers <89836629+YoramTimmers@users.noreply.github.com>
2021-10-11 09:52:38 +0200

    exercise3

---
+++ robotarm-python-2021-main/robotarm-python-2021-main/oefening3.py
@@ -0,0 +1,29 @@
+from RobotArm import RobotArm
+robotArm = RobotArm("exercise 3")
+
+for i in range(1,2):
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    robotArm.moveLeft()
+
+for w in range(1,2):
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    robotArm.moveLeft()
+
+for q in range(1,2):
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    robotArm.moveLeft()
+
+for a in range(1,2):
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+
+
+
+robotArm.wait
